9a50738997e2dd773b4b3522a851e4214174250a
unknown <niikeshrai@gmail.com>
2024-06-24 21:51:34 +0545

    Created forms to inherit from models

--- CoreToDo/views.py
+++ CoreToDo/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
+from .forms import *
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Hello World")
     tasks = Task.objects.all()
-    context = {'tasks':tasks}
+
+    form = TaskForm()
+    context = {'tasks':tasks, 'form':form}
 
     return render(request, 'list.html', context)
