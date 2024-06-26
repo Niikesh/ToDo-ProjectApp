6654385901aa396dfdcc77032a7055a8026b2153
unknown <niikeshrai@gmail.com>
2024-06-26 19:01:23 +0545

    Adding CRUD logic and functionalities

--- CoreToDo/views.py
+++ CoreToDo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 
@@ -10,6 +10,39 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')
+        
     context = {'tasks':tasks, 'form':form}
 
     return render(request, 'list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task) #'instance=task' will show the current list to be updated in new page
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'form':form}
+
+    return render(request, 'update_task.html', context)
+
+
+def delete(request,pk):
+
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item':item}
+    return render(request, 'delete.html', context)
